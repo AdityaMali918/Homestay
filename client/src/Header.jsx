04bb6d504a07793605b5fcc19e2cfbc71694b502
AdityaMali918 <adityamali918@gmail.com>
2023-12-22 12:10:47 +0530

    airbnb

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
           <Link to={'/'} className="flex item-center gap-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90 text-primary">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
           </svg>
-            <span className='font-bold text-xl'>MDbnb</span>
+            <span className='font-bold text-xl'>airbnb</span>
           </Link>
           <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300 font-bold'>
             <div>Anywhere</div>
