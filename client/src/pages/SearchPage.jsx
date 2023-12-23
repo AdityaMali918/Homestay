9439d9e33f9cfbc5eb7cdc8d2493565092603abb
AdityaMali918 <adityamali918@gmail.com>
2023-12-23 12:13:24 +0530

    sucessful search 2

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -40,7 +40,7 @@ export default function SearchPage() {
           className=" border my-2 py-2 px-3 rounded-2xl w-96"
           placeholder="search"
         />
-        <button className="bg-primary p-2  text-white rounded-full w-auto"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <button className="bg-primary p-2  text-white rounded-full w-auto mr-3"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
 </svg>
 </button>
