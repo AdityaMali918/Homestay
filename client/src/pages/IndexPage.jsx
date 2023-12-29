004ce5d019b56c9659ab40230c8095d3150bfaf3
AdityaMali918 <adityamali918@gmail.com>
2023-12-29 13:30:34 +0530

    final commit 3

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -42,7 +42,7 @@ export default function IndexPage() {
                     <div className="m-3 font-bold">
                         Continue exploring
                     </div>
-                    <div className="bg-black rounded-xl text-white mb-3 w-24 cursor-pointer" onClick={handleShowMore}>
+                    <div className="bg-black rounded-xl text-white mb-3 w-full text-center cursor-pointer" onClick={handleShowMore}>
                         Show more
                     </div>
                     <div className="w-full h-5 bg-white">
